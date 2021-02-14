04f28d5e1ce7a8dfa1ea1908f8cbc05f7a57531d
Akshay Kumar M S <akshay.k@thinkpalm.com>
2021-02-15 04:49:11 +0530

    Register button added

--- src/app/register-button/register-button.module.ts
+++ src/app/register-button/register-button.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from "@angular/common";
 
 import { RegisterButtonRoutingModule } from "./register-button-routing.module";
 import { MaterialModule } from "../material-module";
+import { FormsModule } from "@angular/forms";
 
 @NgModule({
   declarations: [RegisterButtonRoutingModule.components],
-  imports: [CommonModule, RegisterButtonRoutingModule, MaterialModule],
+  imports: [
+    CommonModule,
+    RegisterButtonRoutingModule,
+    MaterialModule,
+    FormsModule,
+  ],
 })
 export class RegisterButtonModule {}
